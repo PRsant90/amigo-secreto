26431190a261fe130fe7902bf29140446f3dc3ac
PRsant90 <pabloraul.sf@gmail.com>
2025-02-03 21:39:43 -0300

    Arreglar codigo app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let Amigos = [];
+let amigos = [];
 
 //function asignarTextoElemento(elemento, texto) {
     //let elementoHTML = document.querySelector(elemento)
@@ -8,17 +8,21 @@ let Amigos = [];
 //}
 
 
-function amigosIngresados () {
-    let nombreAmigo = document.getElementById('amigo');
-    if (nombreAmigo === null) {
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let nombreAmigo = inputAmigo.value.trim()
+
+    if (nombreAmigo === "") {
         alert ("Por favor, inserte un nombre")
-    }else{
-          Amigos.push();
-          limpiarcaja ();
-          return; 
-    }    
+        return;
+    }
+    amigos.push(nombreAmigo);
+    console.log (amigos)
+    limpiarcaja();
+          
+        
 }      
 
-function limpiarcaja () {
-    document.querySelector('#amigo') = '';
+function limpiarcaja() {
+    document.querySelector('#amigo').value = '';
 }
