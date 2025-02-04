e23507e5914096f61c38fb8556dd26649b2a8afc
PRsant90 <pabloraul.sf@gmail.com>
2025-02-03 21:12:04 -0300

    "Funciones Amigos y Limpiarcaja

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let Amigos = [];
 
+//function asignarTextoElemento(elemento, texto) {
+    //let elementoHTML = document.querySelector(elemento)
+    //elementoHTML.innerHTML = texto;
+    //return;
+//}
+
+
+function amigosIngresados () {
+    let nombreAmigo = document.getElementById('amigo');
+    if (nombreAmigo === null) {
+        alert ("Por favor, inserte un nombre")
+    }else{
+          Amigos.push();
+          limpiarcaja ();
+          return; 
+    }    
+}      
+
+function limpiarcaja () {
+    document.querySelector('#amigo') = '';
+}
